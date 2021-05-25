4fca24d9cc17d1870e382063cec73d15d3b64ccf
Raphaël <kisuke95@hotmail.fr>
2021-05-25 22:28:06 +0200

    Initialization of the PWA

---
+++ public/sw.js
@@ -0,0 +1,57 @@
+self.addEventListener('install', async event => {
+    console.log('install event')
+});
+  
+self.addEventListener('fetch', async event => {
+    console.log('fetch event')
+});
+
+const cacheName = 'pwa-workit';
+const staticAssets = [
+  './',
+  './app.js'
+];
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open(cacheName); 
+    await cache.addAll(staticAssets); 
+    console.log("YOOO WHAT")
+});
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    event.respondWith(cacheFirst(req));
+    console.log("YOYOYOYOY");
+});
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(req);
+    return cachedResponse || networkFirst(req);
+}
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    if (/.*(json)$/.test(req.url)) {
+        event.respondWith(networkFirst(req));
+    } else {
+        event.respondWith(cacheFirst(req));
+    }
+});
+
+async function networkFirst(req) {
+    const cache = await caches.open(cacheName);
+    try { 
+      const fresh = await fetch(req);
+      cache.put(req, fresh.clone());
+      return fresh;
+    } catch (e) { 
+        const cachedResponse = await cache.match(req);
+        return cachedResponse;
+    }
+}
